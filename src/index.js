c3faa5a8d1a45ec1cffa4c88bc84492bb59eb494
Amanda Rafaela de Oliveira <amandarafaeladeoliveira@gmail.com>
2023-10-17 10:13:46 -0300

    Atualizando code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 let nomeHeroi =  "Capitã Marvel"
-let xpHeroi = 4.002
+let xpHeroi = 10.500
 
 if(xpHeroi <= 1.000){
-console.log("Ferro")
+  xpHeroi = "Ferro"
 }else if(xpHeroi >= 1.001 && xpHeroi <= 2.000){
-console.log("Bronze")
+  xpHeroi = "Bronze"
 }else if(xpHeroi >= 2.001 && xpHeroi <= 5.000){
-    console.log("Prata")
+  xpHeroi = "Prata"
 }else if(xpHeroi >= 6.001 && xpHeroi <= 7.000){
-    console.log("Ouro")
+  xpHeroi = "Ouro"
 }else if(xpHeroi >= 7.001 && xpHeroi <= 8.000){
-    console.log("Platina")
+  xpHeroi = "Platina"
 }else if(xpHeroi >= 8.001 && xpHeroi <= 9.000){
-    console.log("Ascendente")
+  xpHeroi = "Ascendente"
 }else if(xpHeroi >= 9.001 && xpHeroi <= 10.000){
-    console.log("Imortal")
+  xpHeroi = "Imortal"
 }else if(xpHeroi >= 10.001){
-    console.log("Radiante")
+  xpHeroi = "Radiante"
 }else{
-console.log("Você comeria mais que o pica pau")
+console.log("Você não atingiu o número de XP Ideal")
 }
 
-console.log("O Herói :" + nomeHeroi , "está no nível de :" + xpHeroi "de XP")
+console.log("O Herói: " 
+            + nomeHeroi,
+            "Está no Nivel: "
+            + xpHeroi,
+            "de XP")
